b4c2decc257322ba69217d212320f214033275b8
Margaux-code <margaux.langautier@gmail.com>
2022-05-23 16:45:11 +0200

    Affichage php et html

--- Melange.php
+++ Melange.php
@@ -8,88 +8,89 @@
 
 <!-- CODE PHP -->
 <?php
-
 /*
     <?= $title ?> <=> <?php echo $title; ?>
 */
 
-include 'connection.php';
-
 // SI BOUTON Musculation PRESSE 
-$type_cours;
-if(isset($_POST['Button1']))
-{
-    $sql1 = "SELECT * FROM coach WHERE Domaine_coach = 'Musculation'";
+setcookie('connection', false, 0, "", "", false, false);
+include 'connection.php';
+$connect = $_COOKIE['connection'];
+$bouton_presse = false;
+if ($connect) {
+    if (isset($_POST['Button1'])) {
+        $sql1 = "SELECT * FROM coach WHERE Domaine_coach = 'Musculation'";
+        $bouton_presse = true;
+    }
+
+    // SI BOUTON Fitness PRESSE 
+    if (isset($_POST['Button2'])) {
+        $sql1 = "SELECT * FROM coach WHERE Domaine_coach = 'Fitness'";
+        $bouton_presse = true;
+    }
+
+    // SI BOUTON Biking PRESSE 
+    if (isset($_POST['Button3'])) {
+        $sql1 = "SELECT * FROM coach WHERE Domaine_coach = 'Biking'";
+        $bouton_presse = true;
+    }
+
+    // SI BOUTON Cardio-Training PRESSE 
+    if (isset($_POST['Button4'])) {
+        $sql1 = "SELECT * FROM coach WHERE Domaine_coach = 'Cardio-Training'";
+        $bouton_presse = true;
+    }
+
+    // SI BOUTON Cours Collectifs PRESSE 
+    if (isset($_POST['Button5'])) {
+        $sql1 = "SELECT * FROM coach WHERE Domaine_coach = 'Cours Collectifs'";
+        $bouton_presse = true;
+    }
+    if ($bouton_presse) {
+        // ON CHERCHE LES INFORMATIONS DU COACH CORRESPONDANT AU DOMAINE SELECTIONNE PAR L'UTILISATEUR (Bouton)
+        $result1 = mysqli_query($db_handle, $sql1);
+
+        while ($row = mysqli_fetch_assoc($result1)) {
+            $Nom = $row['Nom_coach'];
+            $Prenom = $row['Prenom_coach'];
+            $Domaine = $row['Domaine_coach'];
+            $Bureau = $row['Bureau_coach'];
+            $Tel = $row['Tel_coach'];
+            $Email = $row['Email_coach'];
+        }
+    }
 }
 
-// SI BOUTON Fitness PRESSE 
-if(isset($_POST['Button2']))
-{
-    $sql1 = "SELECT * FROM coach WHERE Domaine_coach = 'Fitness'";
-}
-
-// SI BOUTON Biking PRESSE 
-if(isset($_POST['Button3']))
-{
-    $sql1 = "SELECT * FROM coach WHERE Domaine_coach = 'Biking'";
-}
-
-// SI BOUTON Cardio-Training PRESSE 
-if(isset($_POST['Button4']))
-{
-    $sql1 = "SELECT * FROM coach WHERE Domaine_coach = 'Cardio-Training'";
-}
-
-// SI BOUTON Cours Collectifs PRESSE 
-if(isset($_POST['Button5']))
-{
-    $sql1 = "SELECT * FROM coach WHERE Domaine_coach = 'Cours Collectifs'";
-}
-
-// ON CHERCHE LES INFORMATIONS DU COACH CORRESPONDANT AU DOMAINE SELECTIONNE PAR L'UTILISATEUR (Bouton)
-$result1 = mysqli_query($db_handle, $sql1);
-
-while ($row = mysqli_fetch_assoc($result1)) 
-{
-    $Nom = $row['Nom_coach'];
-    $Prenom = $row['Prenom_coach'];
-    $Domaine = $row['Domaine_coach'];
-    $Bureau = $row['Bureau_coach'];
-    $Tel = $row['Tel_coach'];
-    $Email = $row['Email_coach'];
-
-    echo $Prenom; 
-}
 ?>
 
 <body>
-        <table>
-            <tr>
-                <td>Nom:</td>
-                <td><output value = <?php echo $Nom; ?> ></td>
-            </tr>
-            <tr>
-                <td>Prénom:</td>
-                <td><output value="prenom"></td>
-                
-            </tr>
-            <tr>
-                <td>Domaine:</td>
-                <td><output value="domaine"></td>
-            </tr>
-            <tr>
-                <td>Bureau:</td>
-                <td><output value="bureau"></td>
-            </tr>
-            <tr>
-                <td>Telephone:</td>
-                <td><output value="tel"></td>
-            </tr>
-            <tr>
-                <td>Email:</td>
-                <td><output value="email"></td>
-            </tr>
-        </table>
+    <table>
+        <tr>
+            <td>Nom:</td> 
+            <td><?if($bouton_presse && $connect){ echo $Nom;} ?> </td>
+        </tr>
+        <tr>
+            <td>Prénom:</td>
+            <td><?if($bouton_presse && $connect){ echo $Preonom;} ?></td>
+
+        </tr>
+        <tr>
+            <td>Domaine:</td>
+            <td><?if($bouton_presse && $connect){ echo $Domaine;} ?></td>
+        </tr>
+        <tr>
+            <td>Bureau:</td>
+            <td><?if($bouton_presse && $connect){ echo $Bureau;} ?></td>
+        </tr>
+        <tr>
+            <td>Telephone:</td>
+            <td><?if($bouton_presse && $connect){ echo $Tel;} ?></td>
+        </tr>
+        <tr>
+            <td>Email:</td>
+            <td><?if($bouton_presse && $connect){ echo $Email;} ?></td>
+        </tr>
+    </table>
 </body>
 
 </html>
