ff2379ef1ecae55151cf2cb4ad4a168dd63c98dc
Margaux-code <margaux.langautier@gmail.com>
2022-05-25 18:04:26 +0200

    Lien accueil/ pres coach

---
+++ presentation_coach.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Base</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, text/html; charset=utf-8" />
+    <link rel="stylesheet" href="style_base.css">
+    <link rel="stylesheet" href="style_profil_coach.css">
+    <script type="text/javascript" src="script_base.js"></script>
+    <script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<?php
+
+setcookie('connection', false, 0, "", "", false, false);
+if (isset($_POST['coach1'])) {
+    $coach = 1;
+}
+if (isset($_POST['coach2'])) {
+    $coach = 2;
+}
+if (isset($_POST['coach3'])) {
+    $coach = 3;
+}
+if (isset($_POST['coach4'])) {
+    $coach = 4;
+}
+if (isset($_POST['coach5'])) {
+    $coach = 5;
+}
+if (isset($_POST['coach6'])) {
+    $coach = 6;
+}
+if (isset($_POST['coach7'])) {
+    $coach = 7;
+}
+if (isset($_POST['coach8'])) {
+    $coach = 8;
+}
+if (isset($_POST['coach9'])) {
+    $coach = 9;
+}
+if (isset($_POST['coach10'])) {
+    $coach = 10;
+}
+if (isset($_POST['coach11'])) {
+    $coach = 11;
+}
+
+include 'SqlConDatabase.php';
+$connect = $_COOKIE['connection'];
+
+
+if ($connect) {
+
+    $sql1 = "SELECT * FROM coach WHERE Id_coach = '" . $coach . "'";
+
+    // ON CHERCHE LES INFORMATIONS DU COACH CORRESPONDANT AU DOMAINE SELECTIONNE PAR L'UTILISATEUR (Bouton)
+    $result1 = mysqli_query($db_handle, $sql1);
+
+    while ($row = mysqli_fetch_assoc($result1)) {
+        $Nom = $row['Nom_coach'];
+        $Prenom = $row['Prenom_coach'];
+        $Domaine = $row['Domaine_coach'];
+        $Bureau = $row['Bureau_coach'];
+        $Tel = $row['Tel_coach'];
+        $Email = $row['Email_coach'];
+    }
+}
+?>
+<body>
+    <div class="page" id="page">
+        <div class="haut">
+            <div class="logo_slogan">
+                <div class="logo">
+                    <img id="img_logo" src="Image/Logo_Omnes_Sport_bordure.png">
+                </div>
+                <div class="slogan" id="slogan">
+                    <span class="text_slogan">Omnes Sport</span>
+                </div>
+            </div>
+            <div class="div_button">
+                <button class="nav-button" id="accueil"><a class="nav-page" href="accueil.html">Accueil</a></button>
+                <button class="nav-button" id="parcourir"><a class="nav-page" href="toutParcourir.html">Tout parcourir</a></button>
+                <button class="nav-button" id="rdv"><a class="nav-page" href="#">Rendez vous</a></button>
+            </div>
+            <div class="search-box-co">
+                <div class="search-box">
+                    <input type="text" name="input" id="search-bar" class="search" placeholder="Recherche">
+                    <button type="submit" id="searchbutton" class="search"><i class="iconify"
+                            id="loupe" data-icon="simple-line-icons:magnifier"></i></button>
+                </div>
+                <div class="btnRegLog">
+                    <button class="reg-log" id="reg-log" onclick="openForm()"><i class="iconify"
+                            id="compte" data-icon="uil:user"></i></button>
+                    <script>
+                        "use strict";
+
+                        document.cookie = "user=John"; // update only cookie named 'user'
+                                // alert(document.cookie); // show all cookies
+                    </script>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="milieu" id="content">
+
+        <div class="page_profil">
+        <div class="photo_profil">
+            <?php echo " <img src='Image/Profil_profs/" . $coach . ".jpg' alt = 'Photo de Profil' style='max-width :200px;'>";
+            ?>
+        </div>
+        <div class="description_profil">
+           <h1 id="profil"> Profil de coach : </h1>
+            <?php echo "<p class='presentation'>". $Prenom . " ".$Nom . "</br>  Spécialité : " . $Domaine . "</br> <i> Contact :</i> </br> " . $Tel . "</br>" . $Email . "</br> Vous pouvez trouver son bureau au 1er étage, porte n°" . $Bureau . "</br> </p>";
+            ?>
+        </div>
+        <div class="cv_coach">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
+
+        </div>
+    </div>
+
+        </div>
+
+        <div class="footer" id="footer">
+            <div class="copyright">
+                <p>ceci est un copyright</p>
+            </div>
+            <div class="text">
+                <p>footer</p>
+            </div>
+        </div>
+    </div>
+
+    <!--Form de Connection/Inscription-->
+    <div id="CoIns-window">
+        <div class="section" id="section-form">
+            <div id="img-CoIns"><img id="imgCoIns" src="Image/marbreblanc.jpg" width="100%" height="100%"></div>
+            <div class="CoIns">
+                <div id="closeForm-container">
+                    <button class="closeForm"><i class="iconify" id="close" data-icon="eva:close-circle-outline"
+                            onclick="closeForm()"></i></button>
+                </div>
+
+                <nav class="nav nav-tabs" id="myTab">
+                    <a class="nav-item nav-link active" id="tab-co" href="#Co" data-toggle="tab">Connexion</a>
+                    <a class="nav-item nav-link" id="tab-ins" href="#Ins" data-toggle="tab">Inscription</a>
+                </nav>
+
+                <div class="tab-content">
+                    <div class="tab-pane active" id="Co">
+                        <div class="form-input">
+                            <input type="email" name="mail" class="form-style" placeholder="Votre e-mail" id="mail"
+                                autocomplete="off">
+                            <i class="input-icon uil uil-at"></i>
+                        </div>
+                        <div class="form-input mt-2">
+                            <input type="password" name="mdp" class="form-style" placeholder="Votre Mot de passe"
+                                id="mdp" autocomplete="off">
+                            <i class="input-icon uil uil-lock-alt"></i>
+                        </div>
+                        <input type="submit" class="btnValid" name="validCo" value="Envoyer">
+                        <div class="mdp-forget-container">
+                            <p class="mdp-forget"><a href="#0" class="link">Mot de passe oublié ?</a></p>
+                        </div>
+                    </div>
+
+                    <div class="tab-pane" id="Ins">
+                        <form action="php.php" method="get">
+                            <div class="form-input">
+                                <input type="text" name="nom" class="form-style" placeholder="Votre nom" id="nom"
+                                    autocomplete="off">
+                                <i class="input-icon uil uil-user"></i>
+                            </div>
+                            <div class="form-input">
+                                <input type="email" name="mail" class="form-style" placeholder="Votre e-mail" id="mail"
+                                    autocomplete="off">
+                                <i class="input-icon uil uil-at"></i>
+                            </div>
+                            <div class="form-input">
+                                <input type="password" name="mdp" class="form-style" placeholder="Votre mot de passe"
+                                    id="mdp" autocomplete="off">
+                                <i class="input-icon uil uil-lock-alt"></i>
+                            </div>
+                            <input type="submit" class="btnValid" name="validI" value="Validate">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--Fin du Form de Connection/Inscription-->
+
+    <script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js"></script>
+
+    <script>
+        const options = {
+            bottom: '64px', // default: '32px'
+            right: 'unset', // default: '32px'
+            left: '32px', // default: 'unset'
+            time: '0.5s', // default: '0.3s'
+            mixColor: '#fff', // default: '#fff'
+            backgroundColor: '#fff',  // default: '#fff'
+            buttonColorDark: '#100f2c',  // default: '#100f2c'
+            buttonColorLight: '#fff', // default: '#fff'
+            saveInCookies: true, // default: true,
+            label: '🌓', // default: ''
+            autoMatchOsTheme: true // default: true
+        }
+
+        const darkmode = new Darkmode(options);
+        darkmode.showWidget();
+    </script>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+</body>
