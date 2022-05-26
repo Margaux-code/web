51bf1fd497908d84c8c5a5bb23c79e3497475e73
Margaux-code <margaux.langautier@gmail.com>
2022-05-26 20:08:27 +0200

    Times slot chargement

--- gestionPlanning.php
+++ gestionPlanning.php
@@ -21,14 +21,16 @@ if($connect)
     
     $sql1 = "INSERT INTO planning_coach(Id_coach,planning_coach_date,start_time,end_time,temps_consultation, Status,nbre_time_slot) VALUES (".$IdCoach.",'".$date."','".$HeureDepart."','".$HeureFin."','".$TempConsultation."','Actif',".$time_slot.")";
     $res = mysqli_query($db_handle, $sql1);  
-    $sql3 ="SELECT MAX(Id_planning) FROM planning_coach";
-    $res3=  mysqli_query($db_handle, $sql3); 
-    while ($row_3 =  mysqli_fetch_assoc($res3))
+    $sql3 ="SELECT MAX(Id_planning) AS toto FROM planning_coach";
+    $res3=  mysqli_query($db_handle,$sql3);     
+    while($row_3 =  mysqli_fetch_assoc($res3))
     {
-        echo "wesh";
-        $arrayID = $row_3['Id_planning'];
-        echo $arrayID;
-    }  
+        $IdPlanning = $row_3['toto'];
+        
+
+    }
+ 
+ 
 
     $tamponDebut =$HeureDepart;
     for($i=0;$i<$time_slot;$i++)
@@ -36,7 +38,7 @@ if($connect)
 
        $h4=strtotime($tamponDebut);
        $tamponFin = date('H:i',$h4+$h3);
-        $sql2 = "INSERT INTO time_slot(Id_planning,Heure_debut,heure_fin,Status) VALUES (".$arrayID.",'".$tamponDebut."','".$tamponFin."', 'Actif')";
+        $sql2 = "INSERT INTO time_slot(Id_planning,Heure_debut,heure_fin,Status) VALUES (".$IdPlanning.",'".$tamponDebut."','".$tamponFin."', 'Actif')";
         $res2 = mysqli_query($db_handle, $sql2);  
         $tamponDebut= $tamponFin;
 
@@ -48,6 +50,6 @@ if($connect)
     
 
 }
-//header("location:".  $_SERVER['HTTP_REFERER']);
+header("location:".  $_SERVER['HTTP_REFERER']);
 ?>
 
