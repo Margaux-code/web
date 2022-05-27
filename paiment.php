526d5f032f587af36857940740bda4125ca644ad
Margaux-code <margaux.langautier@gmail.com>
2022-05-27 16:29:24 +0200

    Interface paiement basique

--- paiment.php
+++ paiment.php
@@ -5,14 +5,13 @@
     <title>Base</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, text/html" />
     <meta charset="utf-8" />
-    <link rel="stylesheet"  href="RDV.css">
+    <link rel="stylesheet" href="RDV.css">
     <link rel="stylesheet" media="screen and (min-width: 981px)" href="style_base.css">
     <link rel="stylesheet" media="screen and (max-width: 980px)" href="style_base_mobile.css">
     <script type="text/javascript" src="script_base.js"></script>
     <script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 </head>
 
@@ -36,17 +35,15 @@
             <div class="search-box-co">
                 <div class="search-box">
                     <input type="text" name="input" id="search-bar" class="search" placeholder="Recherche">
-                    <button type="submit" id="searchbutton" class="search"><i class="iconify" id="loupe"
-                            data-icon="simple-line-icons:magnifier"></i></button>
+                    <button type="submit" id="searchbutton" class="search"><i class="iconify" id="loupe" data-icon="simple-line-icons:magnifier"></i></button>
                 </div>
                 <div class="btnRegLog">
-                    <button class="reg-log" id="reg-log" onclick="openForm()"><i class="iconify" id="compte"
-                            data-icon="uil:user"></i></button>
+                    <button class="reg-log" id="reg-log" onclick="openForm()"><i class="iconify" id="compte" data-icon="uil:user"></i></button>
                     <script>
                         "use strict";
 
                         document.cookie = "user=John"; // update only cookie named 'user'
-                                // alert(document.cookie); // show all cookies
+                        // alert(document.cookie); // show all cookies
                     </script>
                 </div>
             </div>
@@ -56,64 +53,31 @@
 
         <div class="milieu" id="content">
 
-        
+            <p class="PresRDV">Veuillez renseigner vos informations bancaires pour prendre une réservation. </br> Vous pouvez annulé à tous moment jusqu'à 72h avant et vous serez remboursé intégralement. </p>
 
-        <div class="PresRDV"> Vous êtes sur la page de réservation d'une séance avec
-            <?php
-            include 'SqlConDatabase.php';
-            $connect = $_COOKIE['connection'];
-            $date = $_POST['date'];
-            $idCoach = $_POST['id_coach'];
-            $sql2 = "SELECT * from coach WHERE Id_coach= '" . $idCoach . "'";
-            $get2 = mysqli_query($db_handle, $sql2);
-            while ($row2 = mysqli_fetch_assoc($get2)) {
-                $NomCoach = $row2['Nom_coach'];
-                $PrenomCoach = $row2['Prenom_coach'];
-                $Domaine = $row2['Domaine_coach'];
-                echo " " . $PrenomCoach . " " . $NomCoach . " </br> Avec pour domaine : " . $Domaine . "</br> Prendre rendez vous le " . $date;
-            }
-            ?>
-        </div>
-        <table class="table table-dark">
-        <thread>
-            <tr>
+            <form id="InfoCarte">
+                <div class="form-group">
+                    <label>Nom du détenteur de la carte </label>
+                    <input type="text" class="form-control" id="inputNomCarte" placeholder="Jean Dupont" required>
+                </div>
+                <div class="form-group">
+                    <label>Numéro de carte </label>
+                    <input type="number" class="form-control" id="inputNumCarte" placeholder="1234 5678 9012 3456" required>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label>Date d'expiration</label>
+                        <input type="date" class="form-control" id="inputDateExpiration" value="2022-06-02" min="2022-06-02" required>
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="inputState">CVV</label>
+                        <input type="number" class="form-control" id="CVV" placeholder="000" required>
+                    </div>
+                </div>
 
-                <th scope="col">Début de la séance </th>
-                <th scope="col">Fin de la séance </th>
-                <th scope="col">Reserver cette séance </th>
-            </tr>
-        </thread>
-        <tbody>
-            <?php
+                <button type="submit" class="btn btn-primary">Reserver cette seance</button>
+            </form>
 
-            if ($connect) {
-                $idPlanning = $_POST['id_planning'];
-
-                $sql1 = "SELECT * from time_slot WHERE Id_planning= '" . $idPlanning . "'AND Status='Actif'";
-                $get1 = mysqli_query($db_handle, $sql1);
-
-                while ($row1 = mysqli_fetch_assoc($get1)) {
-                    echo "<tr>";
-                    $debut = $row1['heure_debut'];
-                    $fin = $row1['heure_fin'];
-                    echo "<td>" . $debut . "</td>";
-                    echo "<td>" . $fin . "</td>";
-                    echo "<td> <form name = '1' action ='paiment.php' method='post'>    
-                    <input type='hidden' name='IdCoach' value=''>
-                    <input type='hidden' name='IdClient' value=''>
-                    <input type='hidden' name='IdTimeSlot' value=''>
-                    <input type='hidden' name='DateRdv' value=''>
-                    <input type='hidden' name='HeureDebut' value=''>
-                <input type='submit' name='bouton' class='a' value='Choisir ce coach'>                    
-                </form> </td>";
-
-                    echo "</tr>";
-                }
-            }
-
-            ?>
-        </tbody>
-    </table>
 
         </div>
 
@@ -133,8 +97,7 @@
             <div id="img-CoIns"><img id="imgCoIns" src="Image/marbreblanc.jpg" width="100%" height="100%"></div>
             <div class="CoIns">
                 <div id="closeForm-container">
-                    <button class="closeForm"><i class="iconify" id="close" data-icon="eva:close-circle-outline"
-                            onclick="closeForm()"></i></button>
+                    <button class="closeForm"><i class="iconify" id="close" data-icon="eva:close-circle-outline" onclick="closeForm()"></i></button>
                 </div>
 
                 <nav class="nav nav-tabs" id="myTab">
@@ -146,13 +109,11 @@
                     <div class="tab-pane active" id="Co">
                         <form action="LoginPage.php" method="post">
                             <div class="form-input">
-                                <input type="email" name="mail" class="form-style" placeholder="Votre e-mail" id="mail"
-                                    autocomplete="off">
+                                <input type="email" name="mail" class="form-style" placeholder="Votre e-mail" id="mail" autocomplete="off">
                                 <i class="input-icon uil uil-at"></i>
                             </div>
                             <div class="form-input mt-2">
-                                <input type="password" name="password" class="form-style" placeholder="Votre Mot de passe"
-                                    id="mdp" autocomplete="off">
+                                <input type="password" name="password" class="form-style" placeholder="Votre Mot de passe" id="mdp" autocomplete="off">
                                 <i class="input-icon uil uil-lock-alt"></i>
                             </div>
                             <input type="submit" class="btnValid" name="Se_Connecter" value="Envoyer">
@@ -165,18 +126,15 @@
                     <div class="tab-pane" id="Ins">
                         <form action="LoginPage.php" method="post">
                             <div class="form-input">
-                                <input type="text" name="nom" class="form-style" placeholder="Votre nom" id="nom"
-                                    autocomplete="off">
+                                <input type="text" name="nom" class="form-style" placeholder="Votre nom" id="nom" autocomplete="off">
                                 <i class="input-icon uil uil-user"></i>
                             </div>
                             <div class="form-input">
-                                <input type="email" name="mail" class="form-style" placeholder="Votre e-mail" id="mail"
-                                    autocomplete="off">
+                                <input type="email" name="mail" class="form-style" placeholder="Votre e-mail" id="mail" autocomplete="off">
                                 <i class="input-icon uil uil-at"></i>
                             </div>
                             <div class="form-input">
-                                <input type="password" name="mdp" class="form-style" placeholder="Votre mot de passe"
-                                    id="mdp" autocomplete="off">
+                                <input type="password" name="mdp" class="form-style" placeholder="Votre mot de passe" id="mdp" autocomplete="off">
                                 <i class="input-icon uil uil-lock-alt"></i>
                             </div>
                             <input type="submit" class="btnValid" name="validI" value="Validate">
@@ -197,8 +155,8 @@
             left: '32px', // default: 'unset'
             time: '0.5s', // default: '0.3s'
             mixColor: '#fff', // default: '#fff'
-            backgroundColor: '#fff',  // default: '#fff'
-            buttonColorDark: '#100f2c',  // default: '#100f2c'
+            backgroundColor: '#fff', // default: '#fff'
+            buttonColorDark: '#100f2c', // default: '#100f2c'
             buttonColorLight: '#fff', // default: '#fff'
             saveInCookies: true, // default: true,
             label: '🌓', // default: ''
@@ -209,13 +167,7 @@
         darkmode.showWidget();
     </script>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
