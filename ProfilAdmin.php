e97aaa12a58662d43f7faf78deb71cb3a9210b1b
FrancisBbrt <francois.bbrt@gmail.com>
2022-05-28 18:24:41 +0200

    Profils

--- ProfilAdmin.php
+++ ProfilAdmin.php
@@ -133,7 +133,7 @@ if (isset($_POST["Modifier"])) {
                     <a class="nav-item nav-link" href="#v-pills-CVCoach" data-toggle="tab">Voir CV Coach</a>
             </div>
 
-            <div class="tab-content">
+            <div class="tab-content" id="body_informations">
                 <div class="tab-pane fade show active" id="v-pills-InfosAdmin">
                 <br><br><br><br>
                 <div class="infos_container">
@@ -158,36 +158,8 @@ if (isset($_POST["Modifier"])) {
                         </div>
                     </div>
                 </div>
-                <div class="tab-pane fade" id="v-pills-AjDispos">
-                </div>
-                <div class="tab-pane fade" id="v-pills-CVCoach">
-                    <?php
-                    $xml_file = 'CV.xml';
-                    $xsl_file = 'CV.xsl';
-                    $dom_object = new DOMDocument();
-                    if (!file_exists($xml_file)) {
-                        exit('failed to open $xml_file');
-                    }
-                    $dom_object->load($xml_file);
-                    $xsl_obj = new DomDocument();
-                    if (!file_exists($xsl_file)) {
-                        exit('failed to open $xsl_file');
-                    }
-                    $xsl_obj->load($xsl_file);
-                    $proc = new XSLTProcessor;
-                    $proc->importStylesheet($xsl_obj);
-                    $html_fragment = $proc->transformToXml($dom_object);
-                    print($html_fragment);
-                    ?>
-                </div>
                 <div class="tab-pane fade" id="v-pills-CreerCoach">
                     <div class="infos_container">
-                        <div class="form-group">
-                            <label class="form-label" for="image">Choisir Image :</label>
-                            <div class="relative">
-                                <input class="form-control" id="image" type="file" accept="image/*" required="" autofocus="" placeholder="Veuillez choisir la photo du coach">
-                            </div>
-                        </div>
                         <div class="form-group">
                             <label class="form-label" for="name">Nom :</label>
                             <div class="relative">
@@ -242,6 +214,100 @@ if (isset($_POST["Modifier"])) {
                                 <input class="bouton" type="submit" name="Envoyer" value="Insérer les données">
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="v-pills-CVCoach">
+                    <?php
+                    $xml_file = 'CV.xml';
+                    $xsl_file = 'CV.xsl';
+                    $dom_object = new DOMDocument();
+                    if (!file_exists($xml_file)) {
+                        exit('failed to open $xml_file');
+                    }
+                    $dom_object->load($xml_file);
+                    $xsl_obj = new DomDocument();
+                    if (!file_exists($xsl_file)) {
+                        exit('failed to open $xsl_file');
+                    }
+                    $xsl_obj->load($xsl_file);
+                    $proc = new XSLTProcessor;
+                    $proc->importStylesheet($xsl_obj);
+                    $html_fragment = $proc->transformToXml($dom_object);
+                    print($html_fragment);
+                    ?>
+                </div>
+                <div class="tab-pane fade" id="v-pills-AjDispos">
+                    <div class="infos_planning">
+                        <div class="table_container">
+                            <?php
+                                include 'SqlConDatabase.php';
+                                $sql3 = "SELECT C.Id_coach, C.Nom_coach, C.Domaine_coach, D.planning_coach_date, D.start_time, D.temps_consultation FROM coach C JOIN planning_coach D ON C.Id_coach=D.Id_coach ORDER BY D.Id_Coach";
+                                //$sql1 = "SELECT *  from coach";
+                                $resulttable = mysqli_query($db_handle, $sql3);
+                                echo "<table>";
+                                echo "<tr>";
+                                echo "<th class='table_th'>" . "Id" . "</th>";
+                                echo "<th class='table_th'>" . "Nom" . "</th>";
+                                echo "<th class='table_th'>" . "Expertise" . "</th>";
+                                echo "<th class='table_th'>" . "Date du RDV" . "</th>";
+                                echo "<th class='table_th'>" . "Heure du RDV" . "</th>";
+                                echo "<th class='table_th'>" . "Durée d'un RDV" . "</th>";
+                                echo "</tr>";
+
+                                //afficher le resultat
+                                while ($data1 = mysqli_fetch_assoc($resulttable)) 
+                                {
+                                    echo "<tr>";
+                                    echo "<td class='table_td'>" . $data1['Id_coach'] . "</td>";
+                                    echo "<td class='table_td'>" . $data1['Nom_coach'] . "</td>";
+                                    echo "<td class='table_td'>" . $data1['Domaine_coach'] . "</td>";
+                                    echo "<td class='table_td'>" . $data1['planning_coach_date'] . "</td>";
+                                    echo "<td class='table_td'>" . $data1['start_time'] . "</td>";
+                                    echo "<td class='table_td'>" . $data1['temps_consultation'] . "</td>";
+                                    echo "</tr>";
+                                }
+                                echo "</table>";
+                            ?>
+                        </div>
+                        <br><br>
+                        <form action="gestionPlanning.php" method="post">
+                            <div class="form-group">
+                                <label class="element-label" for="id_coach" required>Id du coach :</label>
+                                <div class="entree">
+                                <input class="form-control" id="id_coach" type="number" name="IDCoach">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="element-label" for="jour_travail">Date :</label>
+                                <div class="entree">
+                                <input class="form-control" id="jour_travail" type="date" name="JourTravail" min="2022-06-02" max="2022-07-10">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="element-label" for="HeureDepart">Heure de départ :</label>
+                                <div class="entree">
+                                <input class="form-control" id="HeureDepart" type="time" name="HeureDepart">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="element-label" for="HeureFin">Heure de fin :</label>
+                                <div class="entree">
+                                <input class="form-control" id="HeureFin" type="time" name="HeureFin">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="element-label" for="TempsConsult">Durée d'un Rendez-vous :</label>
+                                <div class="entree">
+                                <input class="form-control" id="TempsConsult" type="number" name="TempsConsult" min=01:00 max=04:00> 
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="relative">
+                                    <input class="bouton" type="reset">
+                                    <input class="bouton" type="submit" name="JourRDV" value="Ajouter au planning">
+                                </div>
+                            </div>
+                        </form>
                     </div>
                     <!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mattis eu urna in sollicitudin. Pellentesque pharetra lacinia mollis. Maecenas hendrerit scelerisque vestibulum. Curabitur eu faucibus lorem. Nam sollicitudin arcu sit amet risus volutpat lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec a dui vehicula, malesuada risus eu, imperdiet nisi. Fusce laoreet tellus at est interdum, vel facilisis ante finibus. Nunc id urna et libero ullamcorper pulvinar at ac ipsum. In et vulputate odio. Nam sed lacinia augue. Aliquam sed dolor diam. Nam vestibulum, odio eget ullamcorper fringilla, nibh nisl tempor dolor, ac tincidunt turpis augue a ante. Sed eleifend rutrum velit. Maecenas nec tristique sapien. Pellentesque sit amet finibus est.</p> -->
                 </div>
