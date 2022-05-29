90aedfd376af37b8b56d1b57f1d552a93979aa14
Margaux-code <margaux.langautier@gmail.com>
2022-05-29 21:15:48 +0200

    Update SqlConDatabase.php

--- SqlConDatabase.php
+++ SqlConDatabase.php
@@ -3,6 +3,7 @@
     $database = "omnes_sport";
     $db_handle = mysqli_connect('localhost', 'root', '');
     $conn = mysqli_select_db($db_handle, $database);
+    if($_COOKIE['connectionDB']==NULL)
     setcookie('connectionDB', false,0,"","",false,false);
     if (!$conn) 
     {
@@ -11,6 +12,7 @@
         exit();
     }
     else {
+        if(!$_COOKIE['connectionDB'])
         setcookie('connectionDB', true,0,"","",false,false);
     }
 ?>
